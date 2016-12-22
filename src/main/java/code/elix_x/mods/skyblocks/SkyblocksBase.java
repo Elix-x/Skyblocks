3f216d29c7c59dfdaf062e097c3eeeee85b6d3b1
elix-x <Elix-x@users.noreply.github.com>
2016-12-22 11:24:21 +0100

    0.0.2-alpha - Added recipe
    Added recipe for skyblocks - same as in 1.7.10.

--- src/main/java/code/elix_x/mods/skyblocks/SkyblocksBase.java
+++ src/main/java/code/elix_x/mods/skyblocks/SkyblocksBase.java
@@ -1,11 +1,17 @@
 package code.elix_x.mods.skyblocks;
 
+import java.io.File;
+
 import code.elix_x.excore.utils.mod.IMod;
 import code.elix_x.excore.utils.proxy.IProxy;
+import code.elix_x.excore.utils.recipes.RecipeStringTranslator;
 import code.elix_x.mods.skyblocks.block.SkyBlock;
 import code.elix_x.mods.skyblocks.tile.SkyblockTileEntity;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -43,6 +49,12 @@ public class SkyblocksBase implements IMod<SkyblocksBase, IProxy<SkyblocksBase>>
 		GameRegistry.register((skyblock = new SkyBlock()).setRegistryName(SKYBLOCK));
 		GameRegistry.register(new ItemBlock(skyblock).setRegistryName(SKYBLOCK));
 		GameRegistry.registerTileEntity(SkyblockTileEntity.class, SKYBLOCK.toString());
+
+		File configFile = new File(event.getModConfigurationDirectory(), NAME + ".cfg");
+		Configuration config = new Configuration(configFile, VERSION);
+		config.load();
+		GameRegistry.addRecipe(RecipeStringTranslator.fromString(new ItemStack(skyblock, config.getInt("amount", "CRAFTING", 6, 1, 64, "The amount of skyblocks obtained as result of crafting them")), RecipeStringTranslator.validateFromConfig(config.getStringList("recipe", "CRAFTING", RecipeStringTranslator.toString("GEG", "GSG", "GEG", 'G', "blockGlass", 'E', Items.ENDER_EYE, 'S', "endstone"), "The recipe for skyblock"))));
+		config.save();
 		proxy.preInit(event);
 	}
 
