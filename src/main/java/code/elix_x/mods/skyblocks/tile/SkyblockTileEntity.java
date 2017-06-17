fffda16e4e89027c241e56955200b4fda02b5f4a
elix-x <Elix-x@users.noreply.github.com>
2017-06-17 15:39:38 +0200

    Increased skyblocks render distance
    To infinity and beyond!

--- src/main/java/code/elix_x/mods/skyblocks/tile/SkyblockTileEntity.java
+++ src/main/java/code/elix_x/mods/skyblocks/tile/SkyblockTileEntity.java
@@ -1,7 +1,15 @@
 package code.elix_x.mods.skyblocks.tile;
 
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class SkyblockTileEntity extends TileEntity {
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public double getMaxRenderDistanceSquared(){
+		return Double.POSITIVE_INFINITY;
+	}
 
 }
