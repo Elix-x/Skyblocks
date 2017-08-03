27430048d91f1d3d4f87310b8684b79c4de47ce5
elix-x <Elix-x@users.noreply.github.com>
2017-08-03 15:55:50 +0200

    Added time getter to tile

--- src/main/java/code/elix_x/mods/skyblocks/tile/SkyblockTileEntity.java
+++ src/main/java/code/elix_x/mods/skyblocks/tile/SkyblockTileEntity.java
@@ -1,10 +1,17 @@
 package code.elix_x.mods.skyblocks.tile;
 
+import code.elix_x.mods.skyblocks.block.SkyBlock;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class SkyblockTileEntity extends TileEntity {
+
+	public int getSkyblockTime(){
+		IBlockState state = getBlockType().getStateFromMeta(getBlockMetadata());
+		return (state.getValue(SkyBlock.FIXED) ? 0 : (int) world.getWorldTime()) + state.getValue(SkyBlock.TIME) * SkyBlock.TIMEINTERVAL;
+	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
